5e8888ac4d967f267d1e047e622a56f9167b48bb
vishwanath <jeeragod2004@gmail.com>
2025-11-07 22:44:19 +0530

    leraned about input_ function and string_ manipulation

---
+++ Day2/input_function.py
@@ -0,0 +1,8 @@
+input_#input("what is your name?")
+#print("hello" + "  " + input("what is your name?") )
+
+#input("what is your name?
+#name=input("what is your name?")
+#print("hey",name + ", how are you?")
+
+print("hey"+ " "+input("what is your name?")+","+"how are you")
---
+++ Day2/string_manipulation.py
@@ -0,0 +1,7 @@
+#print("hello world\nhello world")
+#print("hello"+" vishwa")
+
+print("string manipulation exercise")
+print("string concatination done with "+" sign")
+print("hello" + "jenny")
+print("new lines can be created with a backslash and n")
