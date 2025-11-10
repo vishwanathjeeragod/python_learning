326da2524d90ce494074bebdf497a1a85b898a47
vishwanath <jeeragod2004@gmail.com>
2025-11-10 09:44:48 +0530

    i learned about data type and variables in python

---
+++ Day3/data_type.py
@@ -0,0 +1,13 @@
+#var_1=123
+#var_2=10.3
+#print(var_1 + var_2)
+#print(type(var_1))
+#print(type(var_2))
+
+#name='vishwa jeeragod'
+#print(name[5])
+
+name_1="vishwa"
+name_2= "bca student"
+print(name_1  +   name_2)
+
---
+++ Day3/var_in_python.py
@@ -0,0 +1,15 @@
+# calculate lenght
+#name=input("what is your name?")
+#lenght=len(name)
+#print(lenght)
+
+age=21
+marks=85.5
+name="vishwa"
+city="dwd"
+is_student=True
+print("name:", name)
+print("age:", age)
+print("marks:", marks)
+print("city:", city)
+print("is student:", is_student)
