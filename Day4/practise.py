6fca2b1faaa45ae150ea64447392057683fe420d
vishwanath <jeeragod2004@gmail.com>
2025-11-11 08:22:18 +0530

    in day4 i lerned day4 about type costing and one coding exercise

---
+++ Day4/practise.py
@@ -0,0 +1,10 @@
+#number=input("enter a two digits number:")
+#first_digit=number[0]
+#second_digit=number[1]
+
+#print (int(first_digit) + int(second_digit))
+
+num1 = int(input("Enter first 2-digit number: "))
+num2 = int(input("Enter second 2-digit number: "))
+result = add_digits_of_two_numbers(num1,num2)
+print("Sum of all digits:", result)
---
+++ Day4/type_casting.py
@@ -0,0 +1,18 @@
+#length=len("vishwa jeeragod")
+#print("your name has"  +str(length)  + "character")
+
+#print(type(length))
+
+#first_number=input("Enter a first number")
+#second_number=input("Enter second number")
+#sum=int(first_number) + int(second_number)
+#print(sum)
+
+#marks = 80
+#marks_float = float(marks)
+#print(marks_float)
+
+age = 25
+age_str = str(age)
+print( "Age is " +age_str) 
+
