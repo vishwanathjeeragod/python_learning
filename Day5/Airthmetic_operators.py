ae9bef4887d33025a04c4416b00c12405d836e97
vishwanath <jeeragod2004@gmail.com>
2025-11-12 08:40:27 +0530

    in day5 i learned about Airthemetic operators and comparision operator

---
+++ Day5/Airthmetic_operators.py
@@ -0,0 +1,14 @@
+#print(5+2)
+#print(5+2*2/4)
+
+#a=5
+#b=3
+#print(a-b)
+
+
+
+#claculate BMI(body mass index)
+weight=float(input("Enter your weight in kg:"))
+height=int(input("Enter your height in meter:"))
+BMI=weight/(height*height)
+print(BMI)
---
+++ Day5/Comparision_operator.py
@@ -0,0 +1,17 @@
+# assignment operator
+a,b=1,2
+c=a+b
+print(c)
+c += a
+print(c)
+c %= b
+print (c)
+c //= a
+print(c)
+
+#relational operator
+#a=5
+#print(a==5)
+#print(a<=5)
+#print(a>5)
+#print(a != 7 )
