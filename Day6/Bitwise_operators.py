91041f5e983fda25e12e9c29dad9d4eccfa26622
vishwanath <jeeragod2004@gmail.com>
2025-11-13 08:23:49 +0530

    in Day6 i learned about bitwise operator and logical operators in python

---
+++ Day6/Bitwise_operators.py
@@ -0,0 +1,13 @@
+#a=5
+#b=4
+#print(a & b)
+#print(a | b)
+#print(a ^ b)
+#print(~a)
+
+print(26 & 23)
+print(17 | 24)
+print(17 ^ 24)
+print(~45)
+print(62 << 2)
+print(56 >> 3)
---
+++ Day6/logical_operators.py
@@ -0,0 +1,9 @@
+#a,b=4,6
+#print(a<3 and b>7)
+#print(a<5 or b>8)
+
+a=10
+b=20
+print(a<b and a==10)
+print(a>b or b==20)
+print(not(a>b))
