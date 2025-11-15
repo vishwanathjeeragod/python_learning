f41b65756accffad0a75eef5e189c7d032998a9d
vishwanath <jeeragod2004@gmail.com>
2025-11-15 09:32:19 +0530

    In Day8 i learned about f_string and random function

---
+++ Day8/f_string.py
@@ -0,0 +1,12 @@
+#name='Vishwa'
+#age=21
+#height=1.5
+#print("my name is ",name,"i am",age,"yeras old","my heights is",height,"meters:")
+
+
+#using f_string function same example
+name='Vishwa'
+age=21
+height=1.5
+print(f"my name is {name} i am {age}yeras old my heights is{height}meters:")
+print(f"vishwa fathes age is {21*2} year old")
---
+++ Day8/round_function.py
@@ -0,0 +1,12 @@
+#print(round (11.5))
+#print(round(674,-1))
+
+print (round(665,-1))
+print (round(675,-1))
+print (round(-8/3))
+
+print (round(6.75,1))
+print (round(6.85,1))
+print (type(round(665,-1)))
+print (round(8.5))
+print (round(7.5))
